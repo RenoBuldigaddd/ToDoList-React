76ba13a06c50bfbbdf6a47a413d507e10b06968b
MKA <mka@rivne.itstep.org>
2020-08-12 15:29:50 +0300

    first commit

---
+++ src/components/ToDoList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { darkblue } from 'color-name';
+
+export default function ToDoList()
+{
+    return(
+        <ul>
+            <li>
+                1
+            </li>
+            <li>
+                2
+            </li>
+        </ul>
+    )
+}
+
